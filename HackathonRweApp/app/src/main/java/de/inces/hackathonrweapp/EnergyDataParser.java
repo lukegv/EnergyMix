5710ee42f8fae28349796396751a9679ccd7f080
lennart <moinks@googlemail.com>
2015-11-19 22:02:23 +0100

    changed EnergyDataParser so it works with strings

--- HackathonRweApp/app/src/main/java/de/inces/hackathonrweapp/EnergyDataParser.java
+++ HackathonRweApp/app/src/main/java/de/inces/hackathonrweapp/EnergyDataParser.java
@@ -13,26 +13,22 @@ import java.util.List;
 /**
  * Created by Lennart on 19.11.2015.
  */
-public class EnergyDataProvider {
-
-    String sampleString = "\"Area\",\"MTU\",\"Biomass  - Actual Aggregated [MW]\",\"Biomass  - Actual Aggregated [MW]\",\"Fossil Brown coal/Lignite  - Actual Aggregated [MW]\",\"Fossil Brown coal/Lignite  - Actual Aggregated [MW]\",\"Fossil Coal-derived gas  - Actual Aggregated [MW]\",\"Fossil Coal-derived gas  - Actual Aggregated [MW]\",\"Fossil Gas  - Actual Aggregated [MW]\",\"Fossil Gas  - Actual Aggregated [MW]\",\"Fossil Hard coal  - Actual Aggregated [MW]\",\"Fossil Hard coal  - Actual Aggregated [MW]\",\"Fossil Oil  - Actual Aggregated [MW]\",\"Fossil Oil  - Actual Aggregated [MW]\",\"Fossil Oil shale  - Actual Aggregated [MW]\",\"Fossil Oil shale  - Actual Aggregated [MW]\",\"Fossil Peat  - Actual Aggregated [MW]\",\"Fossil Peat  - Actual Aggregated [MW]\",\"Geothermal  - Actual Aggregated [MW]\",\"Geothermal  - Actual Aggregated [MW]\",\"Hydro Pumped Storage  - Actual Aggregated [MW]\",\"Hydro Pumped Storage  - Actual Aggregated [MW]\",\"Hydro Run-of-river and poundage  - Actual Aggregated [MW]\",\"Hydro Run-of-river and poundage  - Actual Aggregated [MW]\",\"Hydro Water Reservoir  - Actual Aggregated [MW]\",\"Hydro Water Reservoir  - Actual Aggregated [MW]\",\"Marine  - Actual Aggregated [MW]\",\"Marine  - Actual Aggregated [MW]\",\"Nuclear  - Actual Aggregated [MW]\",\"Nuclear  - Actual Aggregated [MW]\",\"Other  - Actual Aggregated [MW]\",\"Other  - Actual Aggregated [MW]\",\"Other renewable  - Actual Aggregated [MW]\",\"Other renewable  - Actual Aggregated [MW]\",\"Solar  - Actual Aggregated [MW]\",\"Solar  - Actual Aggregated [MW]\",\"Waste  - Actual Aggregated [MW]\",\"Waste  - Actual Aggregated [MW]\",\"Wind Offshore  - Actual Aggregated [MW]\",\"Wind Offshore  - Actual Aggregated [MW]\",\"Wind Onshore  - Actual Aggregated [MW]\",\"Wind Onshore  - Actual Aggregated [MW]\"\n" +
-            "\"BZN|DE-AT-LU\",\"19.11.2015 00:00 - 19.11.2015 00:15 (CET)\",\"4044\",\"0\",\"10496\",\"0\",\"366\",\"0\",\"2864\",\"0\",\"2777\",\"0\",\"194\",\"0\",\"0\",\"0\",\"0\",\"0\",\"7\",\"0\",\"18\",\"1762\",\"2009\",\"0\",\"277\",\"0\",\"0\",\"0\",\"9865\",\"0\",\"3482\",\"0\",\"37\",\"0\",\"0\",\"0\",\"275\",\"0\",\"2481\",\"0\",\"27251\",\"0\"\n" +
-            "\"BZN|DE-AT-LU\",\"19.11.2015 00:15 - 19.11.2015 00:30 (CET)\",\"4049\",\"0\",\"10338\",\"0\",\"365\",\"0\",\"2845\",\"0\",\"2570\",\"0\",\"194\",\"0\",\"0\",\"0\",\"0\",\"0\",\"7\",\"0\",\"25\",\"2878\",\"2005\",\"0\",\"265\",\"0\",\"0\",\"0\",\"9845\",\"0\",\"4140\",\"0\",\"37\",\"0\",\"0\",\"0\",\"272\",\"0\",\"2479\",\"0\",\"27503\",\"0\"\n" +
-            "\"BZN|DE-AT-LU\",\"19.11.2015 00:30 - 19.11.2015 00:45 (CET)\",\"4047\",\"0\",\"10386\",\"0\",\"391\",\"0\",\"2725\",\"0\",\"2475\",\"0\",\"195\",\"0\",\"0\",\"0\",\"0\",\"0\",\"7\",\"0\",\"22\",\"3630\",\"2002\",\"0\",\"264\",\"0\",\"0\",\"0\",\"9859\",\"0\",\"4074\",\"0\",\"37\",\"0\",\"0\",\"0\",\"277\",\"0\",\"2468\",\"0\",\"27469\",\"0\"\n";
+public class EnergyDataParser {
 
     // indices of data in CSV file
     int minDataIndex = 2;
     int maxDataIndex = 40;
     int incrementIndex = 2;
 
-public EnergyDataProvider() {
+    String csvData;
 
-
-}
+    public EnergyDataParser(String inputData) {
+        csvData = inputData;
+    }
 
     public EnergyDataHolder GetLatestDataSet() {
 
-        CSVReader reader = new CSVReader(new StringReader(sampleString));
+        CSVReader reader = new CSVReader(new StringReader(csvData));
         String [] nextLine;
         String [] lastLine;
 
